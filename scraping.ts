11e4875ea3ebf2a124d75e9d1aaaf87f4e8bca2f
0ashen <iusfilipp@gmail.com>
2020-11-06 12:14:20 +0300

    add ts

--- scraping.ts
+++ scraping.ts
@@ -1,5 +1,22 @@
-const http = require('http');
+const https = require('https');
 
-http.request('http://nebo.live', (inc) => {
-    console.log(inc)
+const options = {
+    hostname: 'nebo.live',
+    port: 443,
+    path: '/',
+    method: 'GET'
+};
+
+const req = https.request(options, (res) => {
+    console.log(res)
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
 });
+
+req.on('error', (e) => {
+    console.error(e);
+});
+
+req.end();
+
